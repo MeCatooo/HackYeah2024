279e75b5775f855054bb6afc62b9bd5670aa8abb
DarkB0B <miecze123@onet.pl>
2024-09-28 19:21:16 +0200

    good algorithm calculations yes

--- SigmaBackend/SigmaBackend/Controllers/DataSectorsController.cs
+++ SigmaBackend/SigmaBackend/Controllers/DataSectorsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices.ComTypes;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SigmaBackend.Controllers
 {
@@ -7,7 +8,7 @@ namespace SigmaBackend.Controllers
     [Route("api/[controller]")]
     public class DataSectorsController : Controller
     {
-        
+
         [HttpGet]
         public IActionResult GetTreesEfficiency()
         {
@@ -21,26 +22,65 @@ namespace SigmaBackend.Controllers
         }
 
         [HttpPost]
-        public IActionResult PostAnkietaResults(params int[] results)
+        public IActionResult PostAnkietaResults(params int[] userAnswers)
         {
-            var wynik = 0;
-            foreach (var result in results)
+
+            double minScore = 6000;
+            double maxScore = 11000;
+
+
+            double score = CalculateScore(userAnswers);
+            double multiplier = CalculateMultiplier(score, minScore, maxScore);
+            double finalResult = score * multiplier;
+            int resultt = ResultNormaliser(finalResult);
+
+            return Ok(resultt);
+        }
+
+
+
+        public record TreeEfficiencyResponse(int age, int carbonReduced);
+
+        public static int ResultNormaliser(double result)
+        {
+            int resultInt = (int)result;
+            //if (resultInt % 100 == 0) 
+            //{
+            //    Random random = new Random();
+            //    int randomValue = random.Next(0, 100); 
+            //    return (resultInt / 100) * 100 + randomValue; 
+
+            //}
+            return resultInt;
+        }
+
+        public static double CalculateScore(int[] answers)
+        {
+            // Sprawdź, czy liczba odpowiedzi jest zgodna z oczekiwaną
+            if (answers.Length != 14)
             {
-                if(result == 1)
-                    wynik =+ result * 500;
-                else if (result == 2)
-                    wynik = +result * 1000;
-                else if (result == 3)
-                    wynik = +result * 1500;
-                else if (result == 4)
-                    wynik = +result * 2500;
+                throw new ArgumentException("Oczekiwano 14 odpowiedzi.");
             }
-            
 
+            // Oblicz całkowity wynik
+            double totalScore = 0;
+            foreach (var answer in answers)
+            {
+                totalScore += answer;
+            }
 
-            return Ok(wynik);
+            return totalScore;
+        }
+
+        public static double CalculateMultiplier(double score, double minScore, double maxScore)
+        {
+            double maxPossibleScore = 14 * 4; // Maksymalny możliwy wynik
+            double normalizedScore = score / maxPossibleScore; // Normalizacja wyniku (0-1)
+
+            // Przeskaluj do przedziału 7000-11000
+            double finalMultiplier = minScore + (normalizedScore * (maxScore - minScore));
+            return finalMultiplier / score; // Zwróć mnożnik
         }
     }
-    
-    public record TreeEfficiencyResponse(int age, int carbonReduced);
 }
+
